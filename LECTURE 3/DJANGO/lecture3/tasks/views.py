52916125fe81ab1c03a4245701032e5a12d3145d
joseortiz1080 <joseortiz1080@gmail.com>
2024-09-15 20:41:09 -0500

    se agregan coponentes del metodo PoST

--- LECTURE 3/DJANGO/lecture3/tasks/views.py
+++ LECTURE 3/DJANGO/lecture3/tasks/views.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 class NewTasksForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -18,6 +20,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request,"task/add.html",{
                 "form":form
